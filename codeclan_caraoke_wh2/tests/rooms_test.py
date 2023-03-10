c63092a87777110d7aedc90f178a1049be123a2f
Andrew Blair <andrewblair@Andrews-MacBook-Air.local>
2023-03-10 15:33:33 +0000

    commit 3

--- codeclan_caraoke_wh2/tests/rooms_test.py
+++ codeclan_caraoke_wh2/tests/rooms_test.py
@@ -3,9 +3,15 @@ from src.rooms import Rooms
 
 class TestRooms(unittest.TestCase):
     def setUp(self):
-        self.room_1("Mellow Vibes", 8)
-        self.room_2("Power Balladsladslads", 10)
-        self.room_3("Broken Air Conditioning", 12)
+        self.room_1 = Rooms("Mellow Vibes", 8)
+        self.room_2 = Rooms("Power Balladsladslads", 10)
+        self.room_3 = Rooms("Broken Air Conditioning", 12)
 
 
         # Create rooms, songs and guests
+
+    def test_room_name(self):
+        self.assertEqual("Power Balladsladslads", self.room_2.name)
+
+    def test_room_capacity(self):
+        self.assertEqual(12, self.room_3.capacity)
